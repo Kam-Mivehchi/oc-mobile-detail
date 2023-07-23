d45742dc2c1669b019ef9b6015963c00dfabb51d
zachcygan <zachcygan@gmail.com>
2023-07-22 17:56:07 -0700

    fixed footer to bottom of page

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -89,7 +89,7 @@ const navigation = {
 
 export default function Footer() {
     return (
-        <footer className="">
+        <footer className="sticky top-[100vh]">
             <div className="mx-auto max-w-7xl overflow-hidden px-6 py-20 sm:py-24 lg:px-8">
                 <nav className="-mb-6 columns-2 sm:flex sm:justify-center sm:space-x-12" aria-label="Footer">
                     {navigation.main.map((item) => (
