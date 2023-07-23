78071ba7fc7a254038c580b8d762770fb0a3e1e6
zachcygan <zachcygan@gmail.com>
2023-07-22 18:05:12 -0700

    changed footer colors

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -94,7 +94,7 @@ export default function Footer() {
                 <nav className="-mb-6 columns-2 sm:flex sm:justify-center sm:space-x-12" aria-label="Footer">
                     {navigation.main.map((item) => (
                         <div key={item.name} className="pb-6">
-                            <a href={item.href} className="text-sm leading-6 text-gray-600 hover:text-gray-900">
+                            <a href={item.href} className="text-sm leading-6 text-white hover:text-gray-900">
                                 {item.name}
                             </a>
                         </div>
@@ -102,7 +102,7 @@ export default function Footer() {
                 </nav>
                 <div className="mt-10 flex justify-center space-x-10">
                     {navigation.social.map((item) => (
-                        <a key={item.name} href={item.href} className="text-gray-400 hover:text-gray-500">
+                        <a key={item.name} href={item.href} className="text-white hover:text-gray-500">
                             <span className="sr-only">{item.name}</span>
                             <item.icon className="h-6 w-6" aria-hidden="true" />
                         </a>
