962b6ca323aa8a4138c4a1c75a7d0784509fe9de
kmivehchi-xRoird <kammivehchi@gmail.com>
2023-07-23 13:54:43 -0700

    create central content file and add content to all service areas page

--- src/app/service-areas/page.tsx
+++ src/app/service-areas/page.tsx
@@ -2,6 +2,9 @@
 
 import React from 'react'
 import { Transition } from '@headlessui/react'
+import { ICityData, cityData, tiers } from '../../content';
+import Link from 'next/link';
+import Image from 'next/image';
 
 function ServiceAreas() {
    return (
@@ -14,7 +17,36 @@ function ServiceAreas() {
          leave="ease-out"
          leaveFrom="translate-x-0 opacity-100"
          leaveTo="-translate-x-full opacity-0">
-         Service Areas
+
+         <h2 className='text-2xl font-bold'>Service Areas</h2>
+
+         {cityData.map((city) => {
+            return (
+               <Link href={city.href}>
+                  <h2>{city.name}</h2>
+                  <p className="">{city.description}</p>
+                  <Image src={city.img} alt={city.name} />
+               </Link>
+            )
+         })}
+         <h2 className='text-2xl font-bold'>Services</h2>
+         {tiers.map((service) => {
+            return (
+               <Link href={service.href}>
+                  <h2>{service.name}</h2>
+                  <p className="">{service.description}</p>
+                  {/* <Image src={service.img} alt={service.name} /> */}
+                  <div className="flex gap-2 text-xs">
+
+                     {service.features.map((feature) => {
+                        return (
+                           <p className="">{feature}</p>
+                        )
+                     })}
+                  </div>
+               </Link>
+            )
+         })}
       </Transition>
    )
 }
