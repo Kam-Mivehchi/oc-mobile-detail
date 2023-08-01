790cb5a7dc6a58467cfd12ab63e6af0d6ea64fca
Kamyar Mivehchi <kammivehchi@gmail.com>
2023-08-01 15:36:04 -0700

    update service page

--- src/components/city-page.tsx
+++ src/components/city-page.tsx
@@ -2,24 +2,19 @@ import * as React from 'react'
 import Image from 'next/image'
 import ServiceCard from './service-card'
 import { ICityData, tiers } from '@/content/'
+
 function CityPage({ data }: { data: ICityData }) {
    return (
       <section
          className='flex flex-col items-center max-w-3xl mx-auto relative p-2'>
          <h1 className="text-3xl font-bold text-transparent bg-clip-text bg-gradient-to-br from-accent to-primary ">{data.name}</h1>
-         <div className=" grid place-content-center w-full image-fade rounded-xl  absolute h-full top-16 " style={{
-            background: `url("${data.img}")`,
-            backgroundRepeat: "no-repeat",
-            backgroundSize: "cover",
-            backgroundPosition: `${data.position}`
+         {/* <div className=" grid place-content-center w-full image-fade rounded-xl  absolute h-full top-16 " > */}
 
-         }} >
-
-            {/* <div className="relative w-full h-24 bg-repeat">
+         {/* <div className="relative w-full h-24 bg-repeat">
 
 <Image src={data.img} alt={`${data.name} background image`} fill className="" />
 </div> */}
-         </div>
+         {/* </div> */}
          <p className="relative">{data.description}</p>
          {/* //list of services */}
          <ul className="grid  p-2 w-full  place-content-center gap-2 grid-cols-1 md:grid-cols-2 ">
